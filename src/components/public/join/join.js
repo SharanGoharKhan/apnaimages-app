81cca7a91eb627e0bc699329a67381f627859696
Sharan Khan <sharan@hireninja.com>
2017-04-10 00:48:22 +0500

    work on social authentication

--- src/components/public/join/join.js
+++ src/components/public/join/join.js
@@ -35,63 +35,8 @@ app.controller("JoinController", function ($scope,$http) {
 			})
 	}
 	$scope.login=function(type)
-	{
-		if(type == 'gmail')
-		{
-			var params = {
-				'clientid': '119418628600-rdimbrg6324ia9sdoe6rbhl4hdlrd9ll.apps.googleusercontent.com',
-				'cookiepolicy': 'single_host_origin',
-				'callback': function(result)
-				{
-					if(result['status']['signed_in'])
-					{
-						var request = gapi.client.plus.people.get (
-							{
-								'userId':'me'
-							}
-						);
-						request.execute(function(resp){
-							$scope.$apply(function(){
-								$scope.gmail.username = resp.displayName;
-								$scope.gmail.email = resp.emails[0].value;
-								$scope.gmail.g_image = resp.image.url;
-							});
-						});
-					}
-				},
-				'approvalprompt': 'force',
-				'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
-			};
-			gapi.auth.signIn(params);
-		}
-		if(type=='facebook')
-		{
-			console.log("facebook login");
-			FB.login(function(response)
-			{
-				if(response.authResponse)
-				{
-					FB.api('/me','GET', {fields: 'email, first_name, name, id, picture'}, function(response)
-					{
-						$scope.$apply(function(){
-								$scope.facebook.username = response.name;
-								$scope.facebook.email = response.email;
-								$scope.facebook.f_image = response.picture.data.url;
-							});
-					});
-				}
-				else
-				{
-					//error
-
-				}
-			}, {
-				scope: 'email, user_likes',
-				return_scope: true
-			});
-		}
-		
-		// FB.getLoginStatus(function(response) {
+	{		
+	 // FB.getLoginStatus(function(response) {
 	 //    statusChangeCallback(response);
 	 //  });
 	}
